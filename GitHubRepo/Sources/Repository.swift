960448883ec34a0bbf652fb94841bd5318ac044b
RAFA <rafa71802937@gmail.com>
2024-07-17 08:10:26 +0900

    feat: JSON 응답을 위한 Repository 구조체 추가
    - API에서 반환된 데이터를 앱 내부에서 사용하기 위해 정의
    - 각 프로퍼티와 JSON 키는 CodingKeys를 통해 매핑

---
+++ GitHubRepo/Sources/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  GitHubRepo
+//
+//  Created by RAFA on 7/17/24.
+//
+
+import Foundation
+
+struct Repository: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, language
+        case stargazersCount = "stargazers_count"
+    }
+}
